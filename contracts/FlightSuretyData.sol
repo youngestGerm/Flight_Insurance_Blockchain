f5174023a0c4586916204bf9eddbea64ef0f9ab6
Aaron Zheng <aarondzheng1@gmail.com>
2019-09-19 08:53:32 -0700

    Initial files.

---
+++ contracts/FlightSuretyData.sol
@@ -0,0 +1,260 @@
+pragma solidity >=0.4.25;
+
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract FlightSuretyData {
+    using SafeMath for uint256;
+
+    /********************************************************************************************/
+    /*                                       DATA VARIABLES                                     */
+    /********************************************************************************************/
+    struct Votes {
+        uint votesCounted;
+        mapping(address => bool) addressFromVotes;
+    }
+    
+    struct Airline {
+        bool exists;
+        bool registered;
+        bool funded; //is this 
+        bytes32[] flightKeys;
+        Votes votes;
+        uint numberOfInsurance;
+    }
+
+    address private contractOwner;                                      // Account used to deploy contract
+    bool private operational = true;                                    // Blocks all state changes throughout the contract if false
+    mapping (address => Airline) private airlines;
+    mapping (address => bool) private authorizedCallers;
+    uint private registeredAirlines = 0;
+    uint private fundedAirlines = 0;
+    uint private airlinesCount = 0;
+    /********************************************************************************************/
+    /*                                       EVENT DEFINITIONS                                  */
+    /********************************************************************************************/
+
+    // event AirlineExists()
+    event AuthorizeCaller(address caller);
+    event AirlineFunded(address airlineAddress, bool exists, bool registered, bool funded, uint fundedCount);
+    /**
+    * @dev Constructor
+    *      The deploying account becomes contractOwner
+    */
+    constructor
+                                ( address firstAirline
+                                ) 
+                                public 
+    {
+        contractOwner = msg.sender;
+        airlines[firstAirline] =  Airline(true, true, false, new bytes32[](0), Votes(0), 0);
+    }   
+
+    /********************************************************************************************/
+    /*                                       FUNCTION MODIFIERS                                 */
+    /********************************************************************************************/
+
+    // Modifiers help avoid duplication of code. They are typically used to validate something
+    // before a function is allowed to be executed.
+
+    /**
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in 
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational() {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
+    }
+
+    /**
+    * @dev Modifier that requires the "ContractOwner" account to be the function caller
+    */
+    modifier requireContractOwner(){
+        require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    modifier requireAirlineRegistration(address airlineCompanyAddress) {
+        require(airlines[airlineCompanyAddress].registered, "Airline is not registered and not part of the program");
+        _;
+    } 
+
+    modifier requireAirlineExists(address _address) {
+        require (airlines[_address].exists, "Airline does not exist");
+        _;
+    }
+    /********************************************************************************************/
+    /*                                       UTILITY FUNCTIONS                                  */
+    /********************************************************************************************/
+
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */      
+    function isOperational() 
+                            public 
+                            view 
+                            returns(bool) {
+        return operational;
+    }
+
+
+    /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */    
+    function setOperatingStatus
+                            (
+                                bool mode
+                            ) 
+                            external
+                            requireContractOwner 
+    {
+        operational = mode;
+    }
+
+    function authorizeCaller(address _address) public requireContractOwner requireIsOperational {
+        authorizedCallers[_address] = true;
+        emit AuthorizeCaller(_address);
+    }
+    function isAuthorized(address _address) public returns(bool) {
+        return authorizedCallers[_address];
+    }
+    function isFunded(address _address) public returns(bool) {
+        return airlines[_address].funded;
+    }
+    function getRegisteredAirlines() public view returns(uint) {
+        return registeredAirlines;
+    }
+    function airlineRegistered(address _address) public view returns(bool) {
+        return airlines[_address].exists;
+    }
+    function getAirlineVotes(address airlineAddress) public requireIsOperational returns(uint) {
+        return airlines[airlineAddress].votes.votesCounted;
+    }
+    function airlineExists(address airlineAddress) public view returns(bool) {
+        return airlines[airlineAddress].exists;
+    } 
+    function numberOfFundedAirlines() public view requireIsOperational returns(uint) {
+        return fundedAirlines;
+    }
+    /********************************************************************************************/
+    /*                                     SMART CONTRACT FUNCTIONS                             */
+    /********************************************************************************************/
+
+   /**
+    * @dev Add an airline to the registration queue
+    *      Can only be called from FlightSuretyApp contract
+    *
+    */   
+    function registerAirline
+                            (   
+                                address airlineAddress, bool registered
+                            )
+                            external
+                            view
+                            requireIsOperational
+
+    {
+        
+        airlines[airlineAddress] = Airline(true, registered, false, new bytes32[](0), Votes(0), 0);
+        airlinesCount.add(1);
+        if (registered) {registeredAirlines.add(1);}
+    }
+
+    function setAirlineRegistered(address _address) public requireIsOperational requireAirlineExists(_address) {
+        require(!airlines[_address].registered, "Airline already registered");
+        airlines[_address].registered = true;
+        registeredAirlines.add(1);
+
+    }
+
+   /**
+    * @dev Buy insurance for a flight
+    *
+    */   
+
+    function buy ()
+                            external
+                            payable
+    {
+    }
+
+    /**
+     *  @dev Credits payouts to insurees
+    */
+    function creditInsurees
+                                (
+                                )
+                                external
+                                pure
+    {
+    }
+
+    /**
+     *  @dev Transfers eligible payout funds to insuree
+     *
+    */
+    function pay
+                            (
+                            )
+                            external
+                            pure
+    {
+    }
+
+   /**
+    * @dev Initial funding for the insurance. Unless there are too many delayed flights
+    *      resulting in insurance payouts, the contract should be self-sustaining
+    *
+    */   
+    function fund
+                            (   
+                                address airlineAddress
+                            )
+                            public
+                            payable
+                            requireIsOperational requireAirlineRegistration(airlineAddress) 
+    {
+        require(msg.value >= 10 ether, "The airline did not pay the minimum requirements to be funded");
+        airlines[airlineAddress].funded = true;
+        fundedAirlines = fundedAirlines.add(1);
+        emit AirlineFunded(airlineAddress, airlines[airlineAddress].exists, airlines[airlineAddress].registered, airlines[airlineAddress].funded, fundedAirlines);
+    }
+
+    function getFlightKey
+                        (
+                            address airline,
+                            string memory flight,
+                            uint256 timestamp
+                        )
+                        pure
+                        internal
+                        returns(bytes32) 
+    {
+        return keccak256(abi.encodePacked(airline, flight, timestamp));
+    }
+
+    function vote (address votingFor, address voter) public requireIsOperational {
+        //They can't vote for themselves
+        require(!airlines[votingFor].votes.addressFromVotes[voter], "Airline already voted");
+        airlines[votingFor].votes.addressFromVotes[voter] = true;
+        airlines[votingFor].votes.votesCounted++;
+    }
+    
+    /**
+    * @dev Fallback function for funding smart contract.
+    *
+    */
+    function() 
+                            external 
+                            payable 
+    {
+        fund(msg.sender);
+    }
+
+
+}
+
---
+++ test/flightSurety.js
@@ -0,0 +1,175 @@
+
+var Test = require('../config/testConfig.js');
+var BigNumber = require('bignumber.js');
+
+contract('Flight Surety Tests', async (accounts) => {
+
+  var config;
+  before('setup contract', async () => {
+    config = await Test.Config(accounts);
+    await config.flightSuretyData.authorizeCaller(config.flightSuretyApp.address);
+  });
+
+  /****************************************************************************************/
+  /* Operations and Settings                                                              */
+  /****************************************************************************************/
+
+  it(`(multiparty) has correct initial isOperational() value`, async function () {
+
+    // Get operating status
+    let status = await config.flightSuretyData.isOperational.call();
+    assert.equal(status, true, "Incorrect initial operating status value");
+
+  });
+
+  it(`(multiparty) can block access to setOperatingStatus() for non-Contract Owner account`, async function () {
+
+      // Ensure that access is denied for non-Contract Owner account
+      let accessDenied = false;
+      try 
+      {
+          await config.flightSuretyData.setOperatingStatus(false, { from: config.testAddresses[2] });
+      }
+      catch(e) {
+          accessDenied = true;
+      }
+      assert.equal(accessDenied, true, "Access not restricted to Contract Owner");
+            
+  });
+
+  it(`(multiparty) can allow access to setOperatingStatus() for Contract Owner account`, async function () {
+
+      // Ensure that access is allowed for Contract Owner account
+      let accessDenied = false;
+      try 
+      {
+          await config.flightSuretyData.setOperatingStatus(false);
+      }
+      catch(e) {
+          accessDenied = true;
+      }
+      assert.equal(accessDenied, false, "Access not restricted to Contract Owner");
+      
+  });
+
+  it(`(multiparty) can block access to functions using requireIsOperational when operating status is false`, async function () {
+
+      await config.flightSuretyData.setOperatingStatus(false);
+
+      let reverted = false;
+      try 
+      {
+          await config.flightSurety.setTestingMode(true);
+      }
+      catch(e) {
+          reverted = true;
+      }
+      assert.equal(reverted, true, "Access not blocked for requireIsOperational");      
+
+      // Set it back for other tests to work
+      await config.flightSuretyData.setOperatingStatus(true);
+
+  });
+
+  it('(airline) cannot register an Airline using registerAirline() if it is not funded', async function() {
+    
+    // ARRANGE
+    let newAirline = accounts[3];
+        
+    try {
+        // await config.flightSuretyApp.fundAirline(config.firstAirline, {from: config.firstAirline, value: web3.utils.toWei('10', "ether")});
+        await config.flightSuretyApp.registerAirline(newAirline, {from: config.firstAirline});
+    } catch(e) {
+        console.log(e);
+        console.log(e.logs[0]);
+        // assert.equal(false, true, "Error when registering the Airline");
+    }
+
+    let result = await config.flightSuretyData.airlineRegistered.call(newAirline); 
+
+    // ASSERT
+    assert.equal(result, false, "Airline should not be able to register another airline if it hasn't provided funding");
+
+  });
+  
+  it('the airline can fund itself', async function() {
+
+    await config.flightSuretyData.AirlineFunded({}, (error, res)=> {
+        console.log(res,error, "airline funded or not, line 98");
+    });
+
+    let startingFundedCount = await config.flightSuretyData.numberOfFundedAirlines.call();    
+    
+    try {
+        await config.flightSuretyApp.fundAirline(config.firstAirline, {from: config.firstAirline});
+    }
+    catch(e) {
+        console.log(e.message, "ERROR");
+    }
+    console.log(accounts[0], `first airline ${config.firstAirline}`, `balance: ${web3.eth.getBalance(config.firstAirline).then(console.log)}`);
+    
+    let endingFundedCount = await config.flightSuretyData.numberOfFundedAirlines.call();
+    
+    assert.equal(endingFundedCount.toNumber(), startingFundedCount.toNumber() + 1 , "funded count did not increase");
+
+
+    let result = await config.flightSuretyData.isFunded.call(config.firstAirlinse); 
+
+    assert.equal(result, true, "Airline funding is not successful");
+  })
+//   it('(airline) can vote for an Airline  voteForAirline() if it is funded', async () => {
+
+
+//     // ARRANGE
+//     let newAirline = accounts[4];
+//     console.log(newAirline);
+//     let startVoteCount = 0;
+//     // ACT
+//     try {
+//       await config.flightSuretyApp.fundAirline(config.firstAirline, {from: config.firstAirline,value: web3.utils.toWei('10', "ether")});
+//       await config.flightSuretyApp.registerAirline(newAirline, {from: config.firstAirline});
+//       startVoteCount = await config.flightSuretyData.getAirlineVotesCount.call(newAirline, {from: config.firstAirline});
+//       console.log(startVoteCount.toNumber());
+//       await config.flightSuretyApp.voteForAirline(newAirline, {from: config.firstAirline});
+//     }
+//     catch(e) {
+//       assert.equal(e, true, "Error in try");
+//     }
+//     let endVoteCount = await config.flightSuretyData.getAirlineVotesCount.call(newAirline, {from: config.firstAirline});
+//     console.log(endVoteCount.toNumber());
+
+//     // ASSERT
+//     assert.equal(endVoteCount.toNumber(), startVoteCount.toNumber() + 1, "Funded airline was not able to vote for a registered airline");
+
+//   });
+
+//   it('5th (airline) will be added but not registered', async () => {
+
+
+//     // ARRANGE
+//     let startingRegistereAirlineCount = await config.flightSuretyData.getRegisteredAirlinesCount.call( {from: config.firstAirline});
+//     let startingExistAirlineCount = await config.flightSuretyData.getExistAirlinesCount.call({from: config.firstAirline});
+
+//     // ACT
+//     try {
+//       await config.flightSuretyApp.fundAirline(config.firstAirline, {from: config.firstAirline,value: web3.utils.toWei('10', "ether")});
+//       await config.flightSuretyApp.registerAirline(accounts[4], {from: config.firstAirline});
+//       await config.flightSuretyApp.registerAirline(accounts[5], {from: config.firstAirline});
+//       await config.flightSuretyApp.registerAirline(accounts[6], {from: config.firstAirline});
+//       await config.flightSuretyApp.registerAirline(accounts[7], {from: config.firstAirline});
+//     }
+//     catch(e) {
+//       assert.equal(e, true, "Error in try");
+//     }
+//     let endingRegistereAirlineCount = await config.flightSuretyData.getRegisteredAirlinesCount.call( {from: config.firstAirline});
+//     let endingExistAirlineCount = await config.flightSuretyData.getExistAirlinesCount.call({from: config.firstAirline});
+
+//     // console.log(startingRegistereAirlineCount.toNumber(), existAirlineCount.toNumber());
+
+//     // ASSERT
+//     assert.equal(startingExistAirlineCount.toNumber(), endingExistAirlineCount.toNumber() - 1 , "Funded airline was not able to vote for a registered airline");
+
+//   });
+
+
+});
